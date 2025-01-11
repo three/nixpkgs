409d8333202e8fc430a1cdea1590745a698a2d6c
Eric Roberts <eric@ericroberts.dev>
2025-01-11 23:56:08 +0000

    run nixfmt

--- pkgs/by-name/ho/hoarder/package.nix
+++ pkgs/by-name/ho/hoarder/package.nix
@@ -21,116 +21,116 @@ let
     hash = "sha256-pDOllWmwA4mpUTUpVvryXR/fQ7VoIT+95ZHDYnTUvDA";
   };
 in
-  stdenv.mkDerivation (finalAttrs: {
-    pname = "hoarder";
-    inherit version;
+stdenv.mkDerivation (finalAttrs: {
+  pname = "hoarder";
+  inherit version;
 
-    src = fetchFromGitHub {
-      owner = "hoarder-app";
-      repo = "hoarder";
-      rev = "v${finalAttrs.version}";
-      sha256 = "sha256-3xgpiqq+BV0a/OlcQiGDt59fYNF+zP0+HPeBCRiZj48=";
-    };
+  src = fetchFromGitHub {
+    owner = "hoarder-app";
+    repo = "hoarder";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-3xgpiqq+BV0a/OlcQiGDt59fYNF+zP0+HPeBCRiZj48=";
+  };
 
-    patches = [
-      ./patches/use-local-font.patch
-      ./patches/fix-migrations-path.patch
-    ];
-    postPatch = ''
-      ln -s ${inter}/share/fonts/truetype ./apps/landing/app/fonts
-      ln -s ${inter}/share/fonts/truetype ./apps/web/app/fonts
-    '';
+  patches = [
+    ./patches/use-local-font.patch
+    ./patches/fix-migrations-path.patch
+  ];
+  postPatch = ''
+    ln -s ${inter}/share/fonts/truetype ./apps/landing/app/fonts
+    ln -s ${inter}/share/fonts/truetype ./apps/web/app/fonts
+  '';
 
-    nativeBuildInputs = [
-      python3
-      nodejs
-      node-gyp
-      pnpm.configHook
-    ];
-    pnpmDeps = pnpm.fetchDeps {
-      inherit (finalAttrs) pname version src;
-      hash = "sha256-F6iW0rjcD2RUfWptMLpDw1Gfa2mbbzxqY2Ey1lYZTU4=";
-    };
-    buildPhase = ''
-      runHook preBuild
+  nativeBuildInputs = [
+    python3
+    nodejs
+    node-gyp
+    pnpm.configHook
+  ];
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-F6iW0rjcD2RUfWptMLpDw1Gfa2mbbzxqY2Ey1lYZTU4=";
+  };
+  buildPhase = ''
+    runHook preBuild
 
-      # Based on matrix-appservice-discord
-      pushd node_modules/better-sqlite3
-      npm run build-release --offline "--nodedir=${srcOnly nodejs}"
-      find build -type f -exec ${removeReferencesTo}/bin/remove-references-to -t "${srcOnly nodejs}" {} \;
-      popd
+    # Based on matrix-appservice-discord
+    pushd node_modules/better-sqlite3
+    npm run build-release --offline "--nodedir=${srcOnly nodejs}"
+    find build -type f -exec ${removeReferencesTo}/bin/remove-references-to -t "${srcOnly nodejs}" {} \;
+    popd
 
-      export CI=true
+    export CI=true
 
-      echo "Compiling apps/web..."
-      pushd apps/web
-      pnpm run build
-      popd
+    echo "Compiling apps/web..."
+    pushd apps/web
+    pnpm run build
+    popd
 
-      echo "Building apps/cli"
-      pushd apps/cli
-      pnpm run build
-      popd
+    echo "Building apps/cli"
+    pushd apps/cli
+    pnpm run build
+    popd
 
-      runHook postBuild
-    '';
+    runHook postBuild
+  '';
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = ''
+    runHook preInstall
 
-      mkdir -p $out/share/doc/hoarder
-      cp README.md LICENSE $out/share/doc/hoarder
+    mkdir -p $out/share/doc/hoarder
+    cp README.md LICENSE $out/share/doc/hoarder
 
-      # Copy necessary files into lib/hoarder while keeping the directory structure
-      LIB_TO_COPY="node_modules apps/web/.next/standalone apps/cli/dist apps/workers packages/db packages/shared packages/trpc"
-      HOARDER_LIB_PATH="$out/lib/hoarder"
-      for DIR in $LIB_TO_COPY; do
-        mkdir -p "$HOARDER_LIB_PATH/$DIR"
-        cp -a $DIR/{.,}* "$HOARDER_LIB_PATH/$DIR"
-        chmod -R u+w "$HOARDER_LIB_PATH/$DIR"
-      done
+    # Copy necessary files into lib/hoarder while keeping the directory structure
+    LIB_TO_COPY="node_modules apps/web/.next/standalone apps/cli/dist apps/workers packages/db packages/shared packages/trpc"
+    HOARDER_LIB_PATH="$out/lib/hoarder"
+    for DIR in $LIB_TO_COPY; do
+      mkdir -p "$HOARDER_LIB_PATH/$DIR"
+      cp -a $DIR/{.,}* "$HOARDER_LIB_PATH/$DIR"
+      chmod -R u+w "$HOARDER_LIB_PATH/$DIR"
+    done
 
-      # NextJS requires static files are copied in a specific way
-      # https://nextjs.org/docs/pages/api-reference/config/next-config-js/output#automatically-copying-traced-files
-      cp -r ./apps/web/public "$HOARDER_LIB_PATH/apps/web/.next/standalone/apps/web/"
-      cp -r ./apps/web/.next/static "$HOARDER_LIB_PATH/apps/web/.next/standalone/apps/web/.next/"
+    # NextJS requires static files are copied in a specific way
+    # https://nextjs.org/docs/pages/api-reference/config/next-config-js/output#automatically-copying-traced-files
+    cp -r ./apps/web/public "$HOARDER_LIB_PATH/apps/web/.next/standalone/apps/web/"
+    cp -r ./apps/web/.next/static "$HOARDER_LIB_PATH/apps/web/.next/standalone/apps/web/.next/"
 
-      # Copy and patch helper scripts
-      for HELPER_SCRIPT in ${./helpers}/*; do
-        HELPER_SCRIPT_NAME="$(basename "$HELPER_SCRIPT")"
-        cp "$HELPER_SCRIPT" "$HOARDER_LIB_PATH/"
-        substituteInPlace "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME" \
-          --replace "HOARDER_LIB_PATH=" "HOARDER_LIB_PATH=$HOARDER_LIB_PATH" \
-          --replace "RELEASE=" "RELEASE=${version}" \
-          --replace "NODEJS=" "NODEJS=${nodejs}"
-        chmod +x "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME"
-        patchShebangs "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME"
-      done
+    # Copy and patch helper scripts
+    for HELPER_SCRIPT in ${./helpers}/*; do
+      HELPER_SCRIPT_NAME="$(basename "$HELPER_SCRIPT")"
+      cp "$HELPER_SCRIPT" "$HOARDER_LIB_PATH/"
+      substituteInPlace "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME" \
+        --replace "HOARDER_LIB_PATH=" "HOARDER_LIB_PATH=$HOARDER_LIB_PATH" \
+        --replace "RELEASE=" "RELEASE=${version}" \
+        --replace "NODEJS=" "NODEJS=${nodejs}"
+      chmod +x "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME"
+      patchShebangs "$HOARDER_LIB_PATH/$HELPER_SCRIPT_NAME"
+    done
 
-      # The cli should be in bin/
-      mkdir -p $out/bin
-      mv "$HOARDER_LIB_PATH/hoarder-cli" $out/bin/
+    # The cli should be in bin/
+    mkdir -p $out/bin
+    mv "$HOARDER_LIB_PATH/hoarder-cli" $out/bin/
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    fixupPhase = ''
-      runHook preFixup
+  fixupPhase = ''
+    runHook preFixup
 
-      # Remove large dependencies that are not necessary during runtime
-      rm -rf $out/lib/hoarder/node_modules/{@next,next,@swc,react-native,monaco-editor,faker,@typescript-eslint,@microsoft,@typescript-eslint,pdfjs-dist}
+    # Remove large dependencies that are not necessary during runtime
+    rm -rf $out/lib/hoarder/node_modules/{@next,next,@swc,react-native,monaco-editor,faker,@typescript-eslint,@microsoft,@typescript-eslint,pdfjs-dist}
 
-      # Remove broken symlinks
-      find $out -type l ! -exec test -e {} \; -delete
+    # Remove broken symlinks
+    find $out -type l ! -exec test -e {} \; -delete
 
-      runHook postFixup
-    '';
+    runHook postFixup
+  '';
 
-    meta = {
-      homepage = "https://github.com/hoarder-app/hoarder";
-      description = "A self-hostable bookmark-everything app with a touch of AI for the data hoarders out there";
-      license = lib.licenses.agpl3Only;
-      maintainers = [];
-      platforms = lib.platforms.linux;
-    };
-  })
+  meta = {
+    homepage = "https://github.com/hoarder-app/hoarder";
+    description = "A self-hostable bookmark-everything app with a touch of AI for the data hoarders out there";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
